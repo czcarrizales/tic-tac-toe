197d7bf9ca4b90abdda0f4dd72f3980811361012
Chazz <czcarrizales@yahoo.com>
2023-03-02 10:58:29 -0800

    creating vite react app for tic tac toe

---
+++ tic-tac-toe/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='container'>
+        <div className='top'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className='middle'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className='bottom'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
