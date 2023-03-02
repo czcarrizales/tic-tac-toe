8e78661eab7b5d634a268bbdbc65c52818c17a7c
Chazz <czcarrizales@yahoo.com>
2023-03-02 11:05:40 -0800

    create boxes and rows

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -3,23 +3,25 @@ import './App.css'
 
 function App() {
 
+  
+
   return (
     <div className="App">
       <div className='container'>
-        <div className='top'>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className='top-row row'>
+          <div className='top-left square'>1</div>
+          <div className='top-middle square'>1</div>
+          <div className='top-right square'>1</div>
         </div>
-        <div className='middle'>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className='middle-row row'>
+          <div className='middle-left square'>1</div>
+          <div className='middle square'>1</div>
+          <div className='middle-right square'>1</div>
         </div>
-        <div className='bottom'>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className='bottom-row row'>
+          <div className='bottom-left square'>1</div>
+          <div className='bottom-middle square'>1</div>
+          <div className='bottom-right square'>1</div>
         </div>
       </div>
     </div>
